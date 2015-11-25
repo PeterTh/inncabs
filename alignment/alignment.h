74ca7e25c4be0dbc642ddb74005bbbcf2f5395f0
AntonBikineev <ant.bikineev@gmail.com>
2015-11-25 10:53:46 -0600

    Using unique_ptr is changed to vector by Pat's request

--- alignment/alignment.h
+++ alignment/alignment.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <memory>
+#include <vector>
 
 void del(int k, int *print_ptr, int *last_print, int *displ);
 void add(int v, int *print_ptr, int *last_print, int *displ);
@@ -131,8 +131,8 @@ int get_matrix(int *matptr, int *xref, int scale) {
 
 void forward_pass(char *ia, char *ib, int n, int m, int *se1, int *se2, int *maxscore, int g, int gh) {
 	int i, j, f, p, t, hh;
-    std::unique_ptr<int[]> HH = std::make_unique<int[]>(MAX_ALN_LENGTH);
-    std::unique_ptr<int[]> DD = std::make_unique<int[]>(MAX_ALN_LENGTH);
+    std::vector<int> HH(MAX_ALN_LENGTH);
+    std::vector<int> DD(MAX_ALN_LENGTH);
 
 	*maxscore  = 0;
 	*se1 = 0;
@@ -171,8 +171,8 @@ void forward_pass(char *ia, char *ib, int n, int m, int *se1, int *se2, int *max
 
 void reverse_pass(char *ia, char *ib, int se1, int se2, int *sb1, int *sb2, int maxscore, int g, int gh) {
 	int i, j, f, p, t, hh, cost;
-    std::unique_ptr<int[]> HH = std::make_unique<int[]>(MAX_ALN_LENGTH);
-    std::unique_ptr<int[]> DD = std::make_unique<int[]>(MAX_ALN_LENGTH);
+    std::vector<int> HH(MAX_ALN_LENGTH);
+    std::vector<int> DD(MAX_ALN_LENGTH);
 
 	cost = 0;
 	*sb1  = 1;
@@ -215,10 +215,10 @@ void reverse_pass(char *ia, char *ib, int se1, int se2, int *sb1, int *sb2, int 
 int diff(int A, int B, int M, int N, int tb, int te, int *print_ptr, int *last_print, int *displ, int seq1, int seq2, int g, int gh) {
 	int i, j, f, e, s, t, hh;
 	int midi, midj, midh, type;
-    std::unique_ptr<int[]> HH = std::make_unique<int[]>(MAX_ALN_LENGTH);
-    std::unique_ptr<int[]> DD = std::make_unique<int[]>(MAX_ALN_LENGTH);
-    std::unique_ptr<int[]> RR = std::make_unique<int[]>(MAX_ALN_LENGTH);
-    std::unique_ptr<int[]> SS = std::make_unique<int[]>(MAX_ALN_LENGTH);
+    std::vector<int> HH(MAX_ALN_LENGTH);
+    std::vector<int> DD(MAX_ALN_LENGTH);
+    std::vector<int> RR(MAX_ALN_LENGTH);
+    std::vector<int> SS(MAX_ALN_LENGTH);
 
 	if (N <= 0) {if (M > 0) del(M, print_ptr, last_print, displ); return( - (int) tbgap(M)); }
 
@@ -412,7 +412,7 @@ int pairalign(const inncabs::launch l) {
 			} else {
 				futures.push_back( inncabs::async(l, [&,i,m,n,si,sj,len1]() mutable {
 					int se1, se2, sb1, sb2, maxscore, seq1, seq2, g, gh, len2;
-                    std::unique_ptr<int[]> displ = std::make_unique<int[]>(2*MAX_ALN_LENGTH+1);
+                    std::vector<int> displ((2 * MAX_ALN_LENGTH) + 1);
 					int print_ptr, last_print;
 
 					for (i = 1, len2 = 0; i <= m; i++) {
@@ -437,8 +437,8 @@ int pairalign(const inncabs::launch l) {
 					print_ptr  = 1;
 					last_print = 0;
 
-					diff(sb1-1, sb2-1, se1-sb1+1, se2-sb2+1, 0, 0, &print_ptr, &last_print, displ.get(), seq1, seq2, g, gh);
-					double mm_score = tracepath(sb1, sb2, &print_ptr, displ.get(), seq1, seq2);
+					diff(sb1-1, sb2-1, se1-sb1+1, se2-sb2+1, 0, 0, &print_ptr, &last_print, displ.data(), seq1, seq2, g, gh);
+					double mm_score = tracepath(sb1, sb2, &print_ptr, displ.data(), seq1, seq2);
 
 					if (len1 == 0 || len2 == 0) mm_score  = 0.0;
 					else                        mm_score /= (double) MIN(len1,len2);
@@ -481,7 +481,7 @@ int pairalign_seq() {
 				seq_output[si*nseqs+sj] = (int) 1.0;
 			} else {
 				int se1, se2, sb1, sb2, maxscore, seq1, seq2, g, gh;
-				int displ[2*MAX_ALN_LENGTH+1];
+                std::vector<int> displ((2 * MAX_ALN_LENGTH) + 1);
 				int print_ptr, last_print;
 
 				for (i = 1, len2 = 0; i <= m; i++) {
@@ -506,8 +506,8 @@ int pairalign_seq() {
 				print_ptr  = 1;
 				last_print = 0;
 
-				diff(sb1-1, sb2-1, se1-sb1+1, se2-sb2+1, 0, 0, &print_ptr, &last_print, displ, seq1, seq2, g, gh);
-				mm_score = tracepath(sb1, sb2, &print_ptr, displ, seq1, seq2);
+				diff(sb1-1, sb2-1, se1-sb1+1, se2-sb2+1, 0, 0, &print_ptr, &last_print, displ.data(), seq1, seq2, g, gh);
+				mm_score = tracepath(sb1, sb2, &print_ptr, displ.data(), seq1, seq2);
 
 				if (len1 == 0 || len2 == 0) mm_score  = 0.0;
 				else                        mm_score /= (double) MIN(len1,len2);
